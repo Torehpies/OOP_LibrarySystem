b3f1f685791fe68783813ae69c6f5082388797dc
Kaithlyn Jillanne <kjillannearquilita@gmail.com>
2024-01-03 21:20:46 +0800

    Added Book Class

---
+++ FINAL PROJECT DRAFTZ(5)/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINAL_PROJECT_DRAFTZ_5_
+{
+    internal class Book 
+    {
+        enum Status
+        {
+            Available,
+            Borrowed
+        }
+        string title;
+        string isbn13;
+        string isbn10;
+        string author;
+        string category;
+        string publisher;
+        string published;
+        Status status = Status.Available;
+    }
+}
