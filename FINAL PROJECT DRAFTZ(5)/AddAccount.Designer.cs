4849092a93f52930eb43c77ef09ea887f1cd711f
Seiffer <88134704+Torehpies@users.noreply.github.com>
2024-05-11 21:42:17 +0800

    UI Update Create Account and Admin Login

--- FINAL PROJECT DRAFTZ(5)/AddAccount.Designer.cs
+++ FINAL PROJECT DRAFTZ(5)/AddAccount.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace FINAL_PROJECT_DRAFTZ_5_
+﻿using CustomControls.RJControls;
+
+namespace FINAL_PROJECT_DRAFTZ_5_
 {
     partial class AddAccount
     {
@@ -31,17 +33,17 @@
             Usertbox1 = new TextBox();
             passwordtbox = new TextBox();
             passwordtbox1 = new TextBox();
-            button1 = new Button();
+            button1 = new CustomButton();
             accountType = new ComboBox();
             label1 = new Label();
-            cancelButton = new Button();
+            cancelButton = new CustomButton();
             SuspendLayout();
             // 
             // Usertbox1
             // 
             Usertbox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             Usertbox1.BorderStyle = BorderStyle.None;
-            Usertbox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Usertbox1.Font = new Font("Quicksand", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
             Usertbox1.Location = new Point(6, 44);
             Usertbox1.Margin = new Padding(0);
             Usertbox1.MinimumSize = new Size(258, 32);
@@ -54,7 +56,7 @@
             // 
             passwordtbox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             passwordtbox.BorderStyle = BorderStyle.None;
-            passwordtbox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            passwordtbox.Font = new Font("Quicksand", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
             passwordtbox.Location = new Point(6, 80);
             passwordtbox.Margin = new Padding(0);
             passwordtbox.MinimumSize = new Size(258, 32);
@@ -68,7 +70,7 @@
             // 
             passwordtbox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             passwordtbox1.BorderStyle = BorderStyle.None;
-            passwordtbox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            passwordtbox1.Font = new Font("Quicksand", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
             passwordtbox1.Location = new Point(6, 116);
             passwordtbox1.Margin = new Padding(0);
             passwordtbox1.MinimumSize = new Size(258, 32);
@@ -81,50 +83,64 @@
             // button1
             // 
             button1.BackColor = Color.FromArgb(34, 34, 59);
+            button1.BackgroundColor = Color.FromArgb(34, 34, 59);
+            button1.BorderColor = Color.FromArgb(34, 34, 59);
+            button1.BorderRadius = 15;
+            button1.BorderSize = 0;
+            button1.FlatAppearance.BorderSize = 0;
             button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Segoe UI", 9.792F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Font = new Font("Quicksand", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
             button1.ForeColor = Color.FromArgb(242, 233, 228);
-            button1.Location = new Point(75, 182);
+            button1.Location = new Point(80, 190);
             button1.Margin = new Padding(2);
             button1.Name = "button1";
-            button1.Size = new Size(160, 30);
+            button1.Size = new Size(150, 30);
             button1.TabIndex = 4;
             button1.Text = "Add Account";
+            button1.TextColor = Color.FromArgb(242, 233, 228);
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // accountType
             // 
             accountType.DisplayMember = "Admin";
+            accountType.Font = new Font("Quicksand", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
             accountType.FormattingEnabled = true;
             accountType.Items.AddRange(new object[] { "Admin", "Standard" });
             accountType.Location = new Point(6, 154);
             accountType.Name = "accountType";
-            accountType.Size = new Size(121, 23);
+            accountType.Size = new Size(121, 31);
             accountType.TabIndex = 3;
             accountType.ValueMember = "Admin";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(70, 10);
+            label1.Font = new Font("Quicksand", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(56, 10);
             label1.Name = "label1";
-            label1.Size = new Size(170, 21);
+            label1.Size = new Size(198, 28);
             label1.TabIndex = 8;
             label1.Text = "Create New Account";
             // 
             // cancelButton
             // 
+            cancelButton.BackColor = Color.Transparent;
+            cancelButton.BackgroundColor = Color.Transparent;
+            cancelButton.BorderColor = Color.FromArgb(34, 34, 59);
+            cancelButton.BorderRadius = 15;
+            cancelButton.BorderSize = 2;
+            cancelButton.FlatAppearance.BorderSize = 0;
             cancelButton.FlatStyle = FlatStyle.Flat;
-            cancelButton.Font = new Font("Segoe UI", 9.792F, FontStyle.Bold, GraphicsUnit.Point);
+            cancelButton.Font = new Font("Quicksand", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
             cancelButton.ForeColor = Color.FromArgb(34, 34, 59);
-            cancelButton.Location = new Point(76, 216);
+            cancelButton.Location = new Point(80, 221);
             cancelButton.Margin = new Padding(2);
             cancelButton.Name = "cancelButton";
-            cancelButton.Size = new Size(158, 30);
+            cancelButton.Size = new Size(150, 30);
             cancelButton.TabIndex = 5;
             cancelButton.Text = "Cancel";
+            cancelButton.TextColor = Color.FromArgb(34, 34, 59);
             cancelButton.UseVisualStyleBackColor = false;
             cancelButton.Click += cancelButton_Click;
             // 
@@ -152,9 +168,9 @@
         private TextBox Usertbox1;
         private TextBox passwordtbox;
         private TextBox passwordtbox1;
-        private Button button1;
+        private CustomButton button1;
         private ComboBox accountType;
         private Label label1;
-        private Button cancelButton;
+        private CustomButton cancelButton;
     }
 }
